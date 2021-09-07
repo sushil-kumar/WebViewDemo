7f6d6cbfd70a94a20b3bd922321e5e58c7ea0c74
sushil.or.k <sushil.or.k@gmail.com>
2021-09-07 19:05:04 +0530

    Initial Commit

---
+++ WebViewDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WebViewDemo
+//
+//  Created by MacBook on 07/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WebViewDemo/WebViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewDemoApp.swift
+//  WebViewDemo
+//
+//  Created by MacBook on 07/09/21.
+//
+
+import SwiftUI
+
+@main
+struct WebViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
